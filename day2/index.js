2926c60ceef7af4e8174b594516c898d788813df
Erik Pantzar <erik@epantzar.se>
2017-12-02 16:55:40 +0100

    Working day2

--- day2/index.js
+++ day2/index.js
@@ -6,12 +6,10 @@ fs.readFile('./input.txt', 'utf8', (err, input) => {
   *  also replacing '\t' with spaces, didnt really compute :/
   *  node v8
   */
-  // console.log(input)
-
   const rows = input.split('\n');
   rows.pop(rows.length)
 
-  const diffrenses = rows.map((row) => {
+  const diffrenses = rows.map(row => {
     const arr = row.split(' ')
     const max = arr.reduce((a,b) => Math.max(a,b))
     const min = arr.reduce((a,b) => Math.min(a,b))
@@ -19,7 +17,29 @@ fs.readFile('./input.txt', 'utf8', (err, input) => {
     return max - min
   })
 
-  // sum of diffrenses
   const sumOfDiffrences = diffrenses.reduce((a,b) => a+b)
-  console.log(sumOfDiffrences)
+  // PART 1 solution console.log(sumOfDiffrences)
+
+
+  // PART 2
+  const divides = rows.map(row => {
+    let dii = []
+
+    const les = rows.map(row => row.split(' '))
+    les.forEach(le => {
+      le.forEach(entry => {
+         for(let i=0; i<le.length; i++) {
+           if(entry!==le[i]) {
+              if(entry/le[i]%1==0) {
+                dii.push(entry/le[i])
+                return entry/le[i]
+              }
+           }
+        }
+      })
+    })
+    return dii
+  })
+  const sumOfDivides = divides[0].reduce((a, b) => a+b)
+  console.log(sumOfDivides)
 });
